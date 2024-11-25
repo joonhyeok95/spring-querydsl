35b891c5373086b26e99761fc1a3df213ea10f54
joonhyeok.lim <dkttkemf@gmail.com>
2024-11-25 17:50:26 +0900

    QueryDSL 초기설정 및 검증하기

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,34 @@
 package study.querydsl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
+    @Autowired
+    EntityManager em;
+    
 	@Test
 	void contextLoads() {
+	    Hello hello = new Hello();
+	    em.persist(hello);
+	    
+	    JPAQueryFactory query = new JPAQueryFactory(em);
+	    
+	    QHello qHello = new QHello("h");
+	    
+	    Hello result = query
+	        .selectFrom(qHello)
+	        .fetchOne();
+	    
 	}
 
 }
